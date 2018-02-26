f1aeae2af2d4ef946318b07e146e107dcd5e5d66
Daniel Peña <digrape07@gmail.com>
2018-02-25 21:41:18 -0400

    Iconic removed. Animejs and Vuejs working correctly.

--- 3dc/wwwroot/js/site.js
+++ 3dc/wwwroot/js/site.js
@@ -1,1 +1,15 @@
 ﻿// Write your JavaScript code.
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+var infiniteLoopAlternate = anime({
+    targets: '#boxes .box',
+    translateX: 250,
+    direction: 'alternate',
+    delay: function (el, i, l) { return i * 1000;},
+    loop: true
+});
