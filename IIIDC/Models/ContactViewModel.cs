d2079c47f032f133b6bedd95f5ca1ca96f53d7e2
Daniel Peña <digrape07@gmail.com>
2018-03-18 17:14:47 -0400

    Syntax in ContactViewModel fixed.

--- IIIDC/Models/ContactViewModel.cs
+++ IIIDC/Models/ContactViewModel.cs
@@ -8,7 +8,7 @@ namespace IIIDC.Models
 {
     public class ContactViewModel
     {
-        [StringLength(maximumLength: = 20, ErrorMessage ="The name is too short")]
+        [StringLength(maximumLength: 20, ErrorMessage ="The name is too short")]
         [Required]
         public string Name { get; set; }
 
